9605132824ae0a99f16a8b213f7ac71f7f8f6299
Documenter.jl <documenter@juliadocs.github.io>
2026-07-10 13:10:26 +0000

    build based on 464e2b0

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"api/#API","page":"API","text":"","title":"API"},{"category":"section","location":"#SpectralSuperpositions.jl","page":"Home","text":"Tiny placeholder documentation for the SpectralSuperpositions.jl package.","title":"SpectralSuperpositions.jl"},{"category":"section","location":"#Quick-Start","page":"Home","text":"using SpectralSuperpositions\n\ngreet()\ngreet(\"Julia\")","title":"Quick Start"},{"category":"section","location":"#API","page":"Home","text":"","title":"API"}]
+}
